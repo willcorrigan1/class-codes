a4941bedc88461b735a3c90d26a1dc770d2364c0
aksaddleback <142381968+aksaddleback@users.noreply.github.com>
2024-09-04 01:44:11 +0000

    sep3 codes

---
+++ S01/p1.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace std; 
+
+int main() { 
+    // First solution: 
+    cout << "Alex\nJones\n24\n";
+    // Second solution: 
+    cout << "Alex" << endl << "Jones" << endl << 24 << endl; 
+    // Third solution: 
+    cout << "Alex" << endl; 
+    cout << "Jones\n";
+    cout << 24 << endl; 
+}
